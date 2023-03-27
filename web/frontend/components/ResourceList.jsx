6d47185abed78c0b19324816c976916a0fd25623
unknown <keyurbavishi56@gmail.com>
2023-03-27 18:22:55 +0530

    ResourceList file updated

--- web/frontend/components/ResourceList.jsx
+++ web/frontend/components/ResourceList.jsx
@@ -1,74 +1,67 @@
-import { useQuery,gql } from '@apollo/client';
+// import { useQuery,gql } from '@apollo/client';
+import gql from 'graphql-tag';
+import {useQuery} from '@apollo/react-hooks';
 import store from 'store-js';
 
 const GET_PRODUCTS_BY_ID = gql`
-query getProducts($ids: [ID!]!) {
-  nodes(ids: $ids) {
-    ... on Product {
-      title
-      handle
-      id
-      images(first: 1) {
-        edges {
-          node {
-            originalSrc
-            altText
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {    
+        title
+        handle
+        id
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+              altText
+            }
           }
         }
-      }
-      variants(first: 1) {
-        edges {
-          node {
-            price
-            id
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
           }
         }
       }
     }
   }
-}
-
 `;
+
 
 
 console.log("GET_PRODUCTS_BY_ID===============>", GET_PRODUCTS_BY_ID);
 
-const store_id = store.get('ids');
 
 function ProductList() {
 
-  const { loading, data, error } = useQuery(GET_PRODUCTS_BY_ID, {
-    variables: { ids : store_id },
-
+  const store_id = store.get('ids');
+  const { loading, data, error, networkStatus } = useQuery(GET_PRODUCTS_BY_ID, {
+    variable: { ids : store_id },
   })
 
-  if (error) {
-    console.log("GraphQL error:", error);
-    return <div>{error.message}</div>;
-  }
+  console.log("fetch the data to useQuery=================",useQuery(GET_PRODUCTS_BY_ID, {
+    variables: { ids : store_id },
+  }))
 
-  console.log("store_get========",{ ids: store.get('ids') });
-  if(loading) return <div>Loading...</div>
-
-  
-  if (!data) return <div>No data found</div>;
-  console.log("data===========",data);
-
+  console.log("Query: ============", GET_PRODUCTS_BY_ID);
+  console.log("Variables: ============", { ids: store_id });
+  console.log("loading======================",loading);
+  console.log("data===================",data);
+  console.log("networkStatus==========", networkStatus);
 
   return (
     <div>
-      
-      { loading ?
-      (
-        <p>Loading ...</p>
-      ) :(
         <h1>This is a Keyur's App</h1>
-      )}
     </div>
     );
 }
 
 export default ProductList;
+
 
 
 
